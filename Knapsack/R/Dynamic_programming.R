4809ee877cfcd421a43551f169fb056160c6eaf2
Eric Kamphuis <erika464@student.liu.se>
2019-10-14 13:56:21 +0200

    Tried to improve. Included the process in the vignette

--- Knapsack/R/Dynamic_programming.R
+++ Knapsack/R/Dynamic_programming.R
@@ -18,27 +18,29 @@
 knapsack_dynamic <- function(x, W) {
   # Start time and memory measurement
   # Rprof(assign("tmp", tempfile(), envir = .GlobalEnv), line.profiling = TRUE , memory.profiling = TRUE)
+
   # Check input parameters
   stopifnot(is.data.frame(x), is.numeric(W), W > 0, colnames(x) == c("w", "v"), min(x$w) > 0, min(x$v) > 0)
-  
+
   # Initialize number of objects and round column of weights
   number_of_objects <- nrow(x)
   x$w <- round(x$w, 0)
-  
+
   # Initialize the matrix with dynamic values
   m <- matrix(0, nrow = number_of_objects + 1, ncol = W + 1)
   rownames(m) <- c(0:number_of_objects)
   colnames(m) <- c(0:W)
-  
+
   # Fill matrix recursively
   for (i in 1:number_of_objects) {
     for (j in 0:W) {
-      
+
       # Check if the current weight is larger than the current weight in the matrix
-      if (x$w[i] > j) {
+      object_weight <- x$w[i]
+      if (object_weight > j) {
         m[i + 1, j + 1] <- m[i, j + 1]
       } else {
-        m[i + 1, j + 1] <- max(m[i, j + 1], m[i, j + 1 - x$w[i]] + x$v[i])
+        m[i + 1, j + 1] <- max(m[i, j + 1], m[i, j + 1 - object_weight] + x$v[i])
       }
     }
   }
@@ -47,22 +49,22 @@ knapsack_dynamic <- function(x, W) {
   current_object <- number_of_objects
   current_weight <- W
   best_combination <- numeric()
-  
+
   # While we are in the object and weight range find the next object to add
   while (current_object > 0 & current_weight > 0) {
     while (m[current_object + 1, current_weight + 1] == m[current_object, current_weight + 1]) {
       current_object <- current_object - 1
-      
+
       # Check bounds
       if (current_object == 0) {
         break
       }
     }
-    
+
     # Check bounds
     if (current_object < 1 | current_weight < 1) {
       break
-      
+
       # Store the object in the best combination
     } else {
       best_combination <- append(best_combination, current_object)
@@ -73,6 +75,7 @@ knapsack_dynamic <- function(x, W) {
 
   # Stop time and memory measurement, get summary with: summaryRprof(tmp, lines = "show", memory = "both")
   # Rprof()
+
   # Return the best combination
   return(list(value = round(m[as.character(number_of_objects), as.character(W)], 0),
               elements = rev(best_combination)))
